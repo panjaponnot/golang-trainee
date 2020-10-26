c19f70ff360d7ed37f699774ff0763d0a4729d81
Santipap Thongmai <santipap.th@inet.co.th>
2020-10-26 16:26:57 +0700

    updare env production

--- env/env.sh
+++ env/env.sh
@@ -10,15 +10,15 @@
 # export DB_HOST=203.154.34.24
 # export DB_USERNAME=onera
 # export DB_NAME=onera
-# export DB_PASSWORD=jPjo8Su3sJknRDulAxcCMPhxVTdBdW3ZHHM6vJZN 
+# export DB_PASSWORD=jPjo8Su3sJknRDulAxcCMPhxVTdBdW3ZHHM6vJZN
 
-export DB_HOST=203.151.56.242
+export DB_HOST=db
 export DB_USERNAME=root
 export DB_NAME=ratingscoring_test1
-export DB_PASSWORD=mis@Pass01 
+export DB_PASSWORD=mis@Pass01
 
-export REDIS_HOST=203.154.34.24
-export REDIS_PORT=80
+export REDIS_HOST=redis
+export REDIS_PORT=6379
 
 export ATTENDANT_TOKEN=DVIdkD7Bg/Gc8z1+O9tcAup44PMHbX1JxQYOLDiyRD4xd9m+IsEW8QoafTzPXWw5Dw==
 export ATTENDANT_TOKEN_TYPE=Bearer
